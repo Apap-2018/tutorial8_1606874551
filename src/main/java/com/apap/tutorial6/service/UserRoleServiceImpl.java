6f3dd246f2277def00e5ded4b282e28b9eb88067
@Ronnavelly <ronnavelly@gmail.com>
2018-11-06 14:22:30 +0700

    first commit tanpa latihan

---
+++ src/main/java/com/apap/tutorial6/service/UserRoleServiceImpl.java
@@ -0,0 +1,31 @@
+package com.apap.tutorial6.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.tutorial6.model.UserRoleModel;
+import com.apap.tutorial6.repository.UserRoleDb;
+
+@Service
+@Transactional
+public class UserRoleServiceImpl implements UserRoleService{
+	@Autowired
+	private UserRoleDb userDb;
+	@Override
+	public UserRoleModel addUser(UserRoleModel user) {
+		String pass = encrypt(user.getPassword());
+		user.setPassword(pass);
+		return userDb.save(user);
+	}
+
+	@Override
+	public String encrypt(String password) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+		return hashedPassword;
+	}
+
+
+}
